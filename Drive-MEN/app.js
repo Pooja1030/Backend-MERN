ac60a367c392316c87d2b2f972c673c4374fcc12
Pooja <poojasawant1030@gmail.com>
2024-12-19 17:07:00 +0530

    implementing drive

---
+++ Drive-MEN/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const userRouter = require('./routes/user.routes')
+const dotenv = require('dotenv');
+
+dotenv.config();
+const connectToDB = require('./config/db')
+connectToDB();
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.user(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
+
+
+app.use('/user', userRouter);
+
+app.listen(3000, () => {
+    console.log("Server is running");
+})
